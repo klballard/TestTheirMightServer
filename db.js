feb0e0c88420bca20ec55689e1ac68d4ea4b7bdf
klballard <kyleleeballard@gmail.com>
2020-11-17 15:03:03 -0500

    test3

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     //host: 'klb-testtheirmight.herokuapp.com',  
-    dialect: 'postgres'
+   // dialect: 'postgres'
 });
 
 sequelize.authenticate().then(
