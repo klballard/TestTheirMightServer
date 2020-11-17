10f915ca942d97033f863a7f92aa6e60926a621b
klballard <kyleleeballard@gmail.com>
2020-11-17 11:21:40 -0500

    first push.5

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    //host: 'localhost',  
+    host: 'localhost',  
     dialect: 'postgres'
 });
 
