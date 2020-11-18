3df5860cc85d091d9919390b31ac52bf4e335aa6
klballard <kyleleeballard@gmail.com>
2020-11-18 13:44:42 -0500

    test8

--- db.js
+++ db.js
@@ -1,15 +1,15 @@
 //DATABASE_URL=postgresql://postgres:c00ki3s@localhost/TestTheirMight
 const Sequelize = require('sequelize');
-/*
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     host: 'localhost',
     dialect: "postgres"
 });
-*/
+/*
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres"
 });
-
+*/
 sequelize.authenticate().then(
     function() {   
         console.log('Connected to testtheirmight postgres database');
