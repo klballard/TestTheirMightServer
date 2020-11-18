62d202a6a90854efa6efe8b16f5e90304418b016
klballard <kyleleeballard@gmail.com>
2020-11-18 10:54:07 -0500

    testing5

--- middleware/validate-session.js
+++ middleware/validate-session.js
@@ -1,28 +1,25 @@
 var jwt = require('jsonwebtoken');
 var sequelize = require('../db');
-var User = require('../models/user')( sequelize, require('sequelize') );
+var User = sequelize.import('../models/user');
 
-module.exports = function(req, res, next) {
-    if (req.method == 'OPTIONS') {
-        next()
-    } else {
-        var sessionToken = req.headers.authorization; 
-        console.log(sessionToken) 
-        if (!sessionToken) return res.status(403).send({ auth: false, message: 'No token provided.'}); 
-    else {
-        jwt.verify(sessionToken, process.env.JWT_SECRET, (err, decoded) => { 
-            if(decoded){
-                User.findOne({where: {id: decoded.id}}).then(user => { 
-                    req.user = user;
-                    next();
-                },
-                function(){ 
-                    res.status(401).send({error: 'Not authorized. No matching user info was found.'});
-                });
-            } else { 
-                res.status(400).send({error: 'Not authorized. No value for decoded info.'});
-            }
-        });
-    }
-}
-}
+const validateSession = (req, res, next) => { 
+    const token = req.headers.authorization;
+    console.log(req.headers)
+    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+        // console.log(`INVALID TOKEN: ${decodedToken}`)
+        if (!err && decodedToken) {
+            User.findOne({ where: {id: decodedToken.id}})
+            .then(user => {
+                if (!user) throw 'err';
+                req.user = user;
+                return next();
+            })
+            .catch(err => {next(err); console.log('this could be where it broke')})
+        } else {
+            req.errors = err;
+            res.status(401).send("This is a bad token")
+        }
+    })
+};
+
+module.exports = validateSession;
