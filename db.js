db57d09e20037636caa71fd591c4b8d6f57410e8
klballard <kyleleeballard@gmail.com>
2020-11-18 12:17:57 -0500

    take3

--- db.js
+++ db.js
@@ -1,6 +1,11 @@
 //DATABASE_URL=postgresql://postgres:c00ki3s@localhost/TestTheirMight
 const Sequelize = require('sequelize');
-
+/*
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+    host: 'localhost',
+    dialect: "postgres"
+});
+*/
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres"
 });
