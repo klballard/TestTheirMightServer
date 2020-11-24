1e5d5aafd08f031f5aadeeb0654c85b151a1159c
klballard <kyleleeballard@gmail.com>
2020-11-24 10:49:33 -0500

    header rollback to hopefully fix cors

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,7 +1,7 @@
 module.exports = function (req, res, next) {
-    res.header('access-control-allow-origin', 'https://testtheirmightheroku.herokuapp.com/');
+    res.header('access-control-allow-origin', '*');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, content-type, application/json');
+    res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     next();
   };
