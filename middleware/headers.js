05a6776b2e8a079e02d4ca6da2bf6a78d272f2c6
klballard <kyleleeballard@gmail.com>
2020-11-24 10:47:32 -0500

    header update2 to hopefully fix cors

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next) {
-    res.header('access-control-allow-origin', '*');
+    res.header('access-control-allow-origin', 'https://testtheirmightheroku.herokuapp.com/');
     res.header("Access-Control-Allow-Credentials", true);
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, content-type, application/json');
