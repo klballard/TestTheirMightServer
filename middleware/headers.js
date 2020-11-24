7ebef6b5681194ee727a78b8ebba4837081fbfc9
klballard <kyleleeballard@gmail.com>
2020-11-24 16:22:12 -0500

    more server tinkering.....

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,6 +1,6 @@
 module.exports = function (req, res, next) {
     if (req.method == "OPTIONS"){
-    res.header('access-control-allow-origin', 'http://localhost:3000');
+    res.header('access-control-allow-origin', '*');
   //  res.header("Access-Control-Allow-Credentials", true);
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
