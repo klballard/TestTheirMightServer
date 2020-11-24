e7fe516b56762596e99dbe29f0a210bfd18cf0fb
klballard <kyleleeballard@gmail.com>
2020-11-24 10:52:12 -0500

    header update3 to hopefully fix cors

--- middleware/headers.js
+++ middleware/headers.js
@@ -1,7 +1,8 @@
 module.exports = function (req, res, next) {
     res.header('access-control-allow-origin', '*');
-    res.header("Access-Control-Allow-Credentials", true);
+    //res.header("Access-Control-Allow-Credentials", true);
     res.header('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('access-control-allow-headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Max-Age', '86400');
     next();
   };
