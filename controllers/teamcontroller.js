1c61c51fb0d43d1ec4b6b72c3f9cc611e1553a28
klballard <kyleleeballard@gmail.com>
2020-12-02 11:52:40 -0500

    edit2

--- controllers/teamcontroller.js
+++ controllers/teamcontroller.js
@@ -116,7 +116,7 @@ router.delete('/:id', function(req, res) {
 //! Get a single team
 
 router.get(':/id', function(req,res) {
-    var data = req.team.id;
+    var data = req.params.id;
     var userId = req.user.id;
 
     TeamModel
