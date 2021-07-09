27d3b129d568fdc60464c60b6aef35bad9433bb5
klballard <kyleleeballard@gmail.com>
2021-07-08 20:54:15 -0400

    more server changes 7

--- models/fighter.js
+++ models/fighter.js
@@ -1,42 +1,4 @@
 
-// const { DataTypes } = require('sequelize');
-// const db = require('../db');
-
-// const Fighter = db.define('fighter', {
-//     fighterName: {
-//         type: DataTypes.STRING,
-//         allowNull: false,
-//     },
-//     intelligence: {
-//         type: DataTypes.INTEGER,
-//     },
-//     strength: {
-//         type: DataTypes.INTEGER,
-//     },
-//     speed: {
-//         type: DataTypes.INTEGER,
-//     },
-//     durability: {
-//         type: DataTypes.INTEGER,
-//     },
-//     power: {
-//         type: DataTypes.INTEGER,
-//     },
-//     combat: {
-//         type: DataTypes.INTEGER,
-//     },
-//     image: {
-//         type: DataTypes.STRING,
-//     },
-//     userId: {
-//         type: DataTypes.INTEGER,
-//     },
-// });
-
-// module.exports = Fighter;
-
-
-
 module.exports = function (sequelize, DataTypes) {
     return sequelize.define('fighter', {
          fighterName: DataTypes.STRING,
